ae1188c8afdc2e3ae7fab93a0178d2b2285750e0
OscarEncinasVerduzco <ing.oscar.encinas@gmail.com>
2020-04-27 23:47:46 -0700

    Login terminado
    Le cambie el idioma a los html tambien

--- public/login.js
+++ public/login.js
@@ -1,4 +1,29 @@
+
 document.addEventListener("DOMContentLoaded", event => {
     db = firebase.firestore();
     almacenamientoRef = firebase.storage().ref();
 })
+
+function logear(){
+
+    const catalogoRef = db.collection('usuario').doc('usuarioUnico');
+    const query = catalogoRef;
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    query.get()
+    .then(function(doc){
+        data = doc.data()
+        if(data.nombre == username && data.contrasena == password){
+            alert('Ingreso corrrectamente')
+            window.location.replace('menuprincipal.html');
+        }
+        else{
+            alert('Usuario o contraseña incorrectos')
+        }
+    }).catch(function(error){
+        console.log(error)
+    });
+   
+}
