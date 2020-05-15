2a9969476ee9ecd8e06a51feb64a3a34fb50fec0
ArturoMascareno <arturomascareno99@gmail.com>
2020-05-14 19:03:03 -0700

    Hacer Funcion logear innaccesible por consola
    Se mejora la seguridad del código js al hacer innaccesible las variables para el usuario a través de la consola

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,3 @@
-var db;
-var almacenamientoRef;
-
 
 document.addEventListener("DOMContentLoaded", event => {
     db = firebase.firestore();
@@ -8,27 +5,29 @@ document.addEventListener("DOMContentLoaded", event => {
 })
 
 function logear(){
+    var logear1 = (function(){
 
-    const catalogoRef = db.collection('usuario').doc('usuarioUnico');
-    const query = catalogoRef;
-
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    var hash = CryptoJS.MD5(password);
-
-    query.get()
-    .then(function(doc){
-        data = doc.data()
-        if(data.nombre == username && data.contrasena == hash){
-            alert('Ingreso corrrectamente')
-            window.location.replace('../menuprincipal.html');
-        }
-        else{
-            alert('Usuario o contraseña incorrectos')
-        }
-    }).catch(function(error){
-        console.log(error)
-    });
+        const catalogoRef = db.collection('usuario').doc('usuarioUnico');
+        const query = catalogoRef;
+    
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+    
+        var hash = CryptoJS.MD5(password);
+    
+        query.get()
+        .then(function(doc){
+            var data = doc.data()
+            if(data.nombre == username && data.contrasena == hash){
+                alert('Ingreso corrrectamente')
+                window.location.replace('../menuprincipal.html');
+            }
+            else{
+                alert('Usuario o contraseña incorrectos')
+            }
+        }).catch(function(error){
+            console.log(error)
+        });
+    }())
 }
 
